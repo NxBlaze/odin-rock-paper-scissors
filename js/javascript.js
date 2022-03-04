ec01982e02f6d388fd54c4635808b47856a925fa
NxBlaze <lotko.lukasz@gmail.com>
2022-03-04 23:45:57 +0100

    Implement hiding side panel explaining rules

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,3 @@
-let playerChoice;
 let computerScore = 0;
 let playerScore = 0;
 
@@ -25,6 +24,9 @@ rpsMoves.addEventListener('click', (e) => {
     playRound(playerSelection(e.target.closest('BUTTON')), computerSelection());
 });
 
+const rulesButton = document.getElementById('btn-rules');
+rulesButton.addEventListener('click', (e) => toggleRules());
+
 function playerSelection(rpsButton) {
   return movesArray.indexOf(rpsButton);
 }
@@ -42,12 +44,13 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function displayScore() {
-  let playerTotal = document.getElementById('player-score');
-  let computerTotal = document.getElementById('computer-score');
+  const playerTotal = document.getElementById('player-score');
+  const computerTotal = document.getElementById('computer-score');
 
   playerTotal.innerHTML = `<p>PLAYER:<br>${playerScore}</p>`;
   computerTotal.innerHTML = `<p>COMPUTER:<br>${computerScore}</p>`;
 }
+
 // Display current round's selections
 function gameStage(playerSelection, computerSelection) {
   let stage = document.getElementById('game-stage');
@@ -78,63 +81,17 @@ function getResult(playerSelection, computerSelection) {
     computerScore++;
   }
 }
-// game();
 
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     playerSelection = prompt(
-//       "What's your play? Type in rock / paper / scissors"
-//     );
-
-//     if (!inputValidation(playerSelection)) {
-//       // Repeat the round if player's input is invalid
-//       i--;
-//     } else {
-//       console.log(playRound(playerSelection, computerPlay()));
-//     }
-//   }
-
-//   printWinner();
-// }
-
-// function inputValidation(string) {
-//   if (
-//     string === null ||
-//     !(
-//       string.toLowerCase() === 'rock' ||
-//       string.toLowerCase() === 'paper' ||
-//       string.toLowerCase() === 'scissors'
-//     )
-//   ) {
-//     alert('Please enter rock / paper / scissors');
-//     return false;
-//   } else {
-//     return true;
-//   }
-// }
-
-// // Determine the round winner using modulo function,
-// // more info in README.md
-// function playRound(playerSelection, computerValue) {
-//   let playerValue = MOVES.indexOf(playerSelection.toLowerCase());
-
-//   if ((playerValue + 1) % 3 === computerValue) {
-//     computerScore++;
-//     return `You Lose, ${MOVES[computerValue]} > ${MOVES[playerValue]}!`;
-//   } else if (playerValue === computerValue) {
-//     return `It's a Tie - ${MOVES[playerValue]} vs ${MOVES[computerValue]}`;
-//   } else {
-//     playerScore++;
-//     return `You win, ${MOVES[playerValue]} > ${MOVES[computerValue]}`;
-//   }
-// }
-
-// function printWinner() {
-//   if (playerScore > computerScore) {
-//     console.log(`You won, scoring ${playerScore} to ${computerScore}!`);
-//   } else if (playerScore < computerScore) {
-//     console.log(`Computer won, scoring ${computerScore} to ${playerScore}!`);
-//   } else {
-//     console.log(`It's a tie! ${computerScore} to ${playerScore}!`);
-//   }
-// }
+function toggleRules() {
+  let rules = document.getElementById('rules');
+  if (rules.style.display !== 'none') {
+    rules.style.display = 'none';
+    rulesButton.style.left = '-80px';
+    rulesButton.textContent = 'Show Rules';
+  } else {
+    1;
+    rules.style.display = '';
+    rulesButton.style.left = '230px';
+    rulesButton.textContent = 'Hide Rules';
+  }
+}
